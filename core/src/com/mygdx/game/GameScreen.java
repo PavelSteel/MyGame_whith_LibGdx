4a3062e234913b1192e3bdf6ac44edea2eebcf86
PavelSteel <misskiisteel@gmail.com>
2023-04-16 22:02:20 +0300

    Created GameScreen and add viewport

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,21 +1,39 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
+    private MyGdxGame runnerGame;
+    private SpriteBatch batch;
+    private Texture textureBackground;
+
+    public GameScreen(MyGdxGame runnerGame, SpriteBatch batch) {
+        this.runnerGame = runnerGame;
+        this.batch = batch;
+    }
+
     @Override
     public void show() {
-
+        textureBackground = new Texture("bg.png");
     }
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(textureBackground, 0, 0);
+        batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        runnerGame.getViewport().update(width, height, true);
+        runnerGame.getViewport().apply();
     }
 
     @Override
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,41 +3,37 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MyGdxGame extends Game {
-    SpriteBatch batch;
-    Texture img;
-    float vx;
-    float x;
+    private SpriteBatch batch;
+    private GameScreen gameScreen;
+    private Viewport viewport;
+
+    public Viewport getViewport() {
+        return viewport;
+    }
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        vx = 300.0f;
-        x = 0.0f;
+        gameScreen = new GameScreen(this, batch);
+        viewport = new FitViewport(1280, 720);
+        setScreen(gameScreen);
     }
 
     @Override
     public void render() {
         float dt = Gdx.graphics.getDeltaTime();
-        update(dt);
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, x, 0);
-        batch.end();
+        getScreen().render(dt);
     }
 
-    public void update(float dt) {
-        x += vx * dt;
-    }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        getScreen().dispose();
     }
 }
